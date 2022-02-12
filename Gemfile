65a3225da8b23ed612b0ddd25dd95e07b8f8e1e3
sheikhazhar96@gmail.com <sheikhazhar96@gmail.com>
2022-02-12 15:16:12 +0530

    added images

--- Gemfile
+++ Gemfile
@@ -74,3 +74,5 @@ end
 gem "devise"
 
 # gem 'activeadmin'
+gem 'mini_magick', '>= 4.9.5'
+
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+
+  has_one_attached :image
+ 
+
+	#validates :title, :description, :author, :b_subject, :image, :attachment, presence: true
+
 end
