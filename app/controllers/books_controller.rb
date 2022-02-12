aca6384fd501fa121ff56ab98e609cdb603c73c3
sheikhazhar96@gmail.com <sheikhazhar96@gmail.com>
2022-02-12 14:48:18 +0530

    added columns

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -65,6 +65,6 @@ class BooksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def book_params
-      params.require(:book).permit(:title, :description)
+      params.require(:book).permit(:title, :description, :author, :b_subject)
     end
 end
